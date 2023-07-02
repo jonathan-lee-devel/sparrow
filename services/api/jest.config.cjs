fbdd40837e737aa4d6567336684086ebc141e431
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-07-02 17:53:15 +0100

    Attempting to modify config to collect coverage on js for ts

--- services/api/jest.config.cjs
+++ services/api/jest.config.cjs
@@ -1,8 +1,4 @@
 module.exports = {
-    preset: 'ts-jest/presets/default-esm',
-    transform: {
-        '^.+\\.(js|jsx)$': ['ts-jest', {'useESM': true}]
-    },
     testEnvironment: 'node',
     collectCoverage: true,
     testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
