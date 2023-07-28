f545ce8e7bed699eb248cd3b2f44f427a7a83198
Jonathan Lee <43010048+jonathan-lee-devel@users.noreply.github.com>
2023-07-28 03:17:03 +0100

    Migration from old API config to new boilerplate setup (#20)
    * Initial migration
    
    * Tidied up, corrected failing tests in api-old
    
    * Updated github workflows
    
    * Updated working directory for Yarn in GitHub actions workflow
    
    * Added cache-dependency-path for Node/Yarn
    
    * Removed unused boilerplate code

---
+++ services/api/src/routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+
+const swaggerUiOptions = {
+  customCss: '.swagger-ui .topbar { display: none }'
+};
+
+const router = Router();
+
+const SWAGGER_YAML_FILEPATH = path.join(__dirname, '../openapi.yml');
+
+// Dev routes
+if (process.env.NODE_ENV === 'development') {
+  const swaggerYaml = yaml.load(fs.readFileSync(SWAGGER_YAML_FILEPATH, 'utf8')) as Object;
+  router.use('/dev/api-docs', swaggerUi.serve as any);
+  router.get('/dev/api-docs', swaggerUi.setup(swaggerYaml, swaggerUiOptions) as any);
+}
+
+export default router;
