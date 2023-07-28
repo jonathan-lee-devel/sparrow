beb892e9b2abbba881bcdd0daae90742aad91306
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-07-28 20:50:28 +0100

    Updated new API with passport config

---
+++ services/api/src/controllers/auth/login/post.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+import { IUser } from '../../../models/users/User';
+import { HttpStatus } from '../../../common/enums/HttpStatus';
+import logger from '../../../logger';
+import requestMiddleware from '../../../middleware/request-middleware';
+
+export const makePostHandler = () => async (req: Request, res: Response, next: NextFunction) => {
+  // eslint-disable-next-line consistent-return
+  passport.authenticate('local', (err: any, user: IUser, _: any) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({ loginStatus: 'FAILURE' });
+    }
+
+    req.login(user, loginError => {
+      if (loginError) {
+        return next(loginError);
+      }
+      logger.info(`Successful login for user with e-mail: <${user.email}>`);
+      return res.status(HttpStatus.OK).json({
+        loginStatus: 'SUCCESS',
+        user: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email
+        }
+      });
+    });
+  })(req, res, next);
+};
+
+export default requestMiddleware(makePostHandler(), { requiresAuthentication: false });
