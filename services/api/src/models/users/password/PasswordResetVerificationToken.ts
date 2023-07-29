1311324663a9819d7656f09137bfd8d727a64e35
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-07-29 23:40:58 +0100

    Implemented password reset verification token model

---
+++ services/api/src/models/users/password/PasswordResetVerificationToken.ts
@@ -0,0 +1,19 @@
+import { model, Model, Schema } from 'mongoose';
+
+export interface IPasswordResetVerificationToken extends Document {
+  value: string;
+  expiryDate: Date;
+  userEmail: string;
+}
+
+export interface IPasswordResetVerificationTokenModel extends Model<IPasswordResetVerificationToken> {}
+
+const schema = new Schema<IPasswordResetVerificationToken>({
+  value: { type: String, required: true, unique: true },
+  expiryDate: { type: Date, required: true, unique: false },
+  userEmail: { type: String, required: true, unique: true }
+});
+
+const PasswordResetVerificationToken: IPasswordResetVerificationTokenModel = model<IPasswordResetVerificationToken, IPasswordResetVerificationTokenModel>('PasswordResetVerificationToken', schema);
+
+export default PasswordResetVerificationToken;
