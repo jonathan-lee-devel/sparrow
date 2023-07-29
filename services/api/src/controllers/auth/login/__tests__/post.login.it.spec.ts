79c9183d3e0894bf0483b84b2dac4875cd769ff0
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-07-29 01:59:50 +0100

    Added test for internal server error (Login IT)

--- services/api/src/controllers/auth/login/__tests__/post.login.it.spec.ts
+++ services/api/src/controllers/auth/login/__tests__/post.login.it.spec.ts
@@ -6,7 +6,14 @@ jest.mock('../../../../models/users/User');
 
 describe('Test POST Login', () => {
   test('POST /auth/login with no details should return 401 Unauthorized', done => {
-    request(app).post('/auth/login')
+    request(app)
+      .post('/auth/login')
       .expect(HttpStatus.UNAUTHORIZED, done);
+  });
+  test('POST /auth/login with incorrect config should return 500 Internal Server Error', done => {
+    request(app)
+      .post('/auth/login')
+      .send({ username: 'jonathan.lee.devel@gmail.com', password: 'password' })
+      .expect(HttpStatus.INTERNAL_SERVER_ERROR, done);
   });
 });
