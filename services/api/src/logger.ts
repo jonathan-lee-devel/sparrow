d1cbc7a7fdcb647ba69e5fa6bfe891269563dba5
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-08-19 01:23:12 +0100

    Removed file logger

--- services/api/src/logger.ts
+++ services/api/src/logger.ts
@@ -1,31 +1,10 @@
-import {createLogger, format, transports} from 'winston';
+import {createLogger, format} from 'winston';
 import ConsoleLoggerTransport from './lib/winston-console-transport';
 import {environment} from './environment';
 
-const logTransports = [
-  new transports.File({
-    level: 'error',
-    filename: './logs/error.log',
-    format: format.json({
-      replacer: (key, value) => {
-        if (key === 'error') {
-          return {
-            message: (value as Error).message,
-            stack: (value as Error).stack,
-          };
-        }
-        return value;
-      },
-    }),
-  }),
-  new ConsoleLoggerTransport(),
-];
-
 const logger = createLogger({
-  format: format.combine(
-      format.timestamp(),
-  ),
-  transports: logTransports,
+  format: format.combine(format.timestamp()),
+  transports: [new ConsoleLoggerTransport()],
   defaultMeta: {service: 'api'},
   level: environment.NODE_ENV === 'development' ? 'silly' : 'info',
 });
