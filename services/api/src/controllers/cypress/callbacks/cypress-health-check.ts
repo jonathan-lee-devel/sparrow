96e95830930f39cd78f0edf8d8cac489e0879ea8
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-08-27 02:46:42 +0100

    Added IP to logs of Cypress health check

--- services/api/src/controllers/cypress/callbacks/cypress-health-check.ts
+++ services/api/src/controllers/cypress/callbacks/cypress-health-check.ts
@@ -6,6 +6,6 @@ import {HttpStatus} from '../../../lib/enums/HttpStatus';
 export const makeCypressHealthCheckCallback = (
     logger: winston.Logger,
 ): AnonymousEndpointCallback<CypressHealthCheckRequestBody, CypressHealthCheckRequestQuery> => async (req, res) => {
-  logger.info(`Cypress requested health check`);
+  logger.info(`Cypress requested health check from ${req.ip}`);
   return res.status(HttpStatus.OK).send();
 };
