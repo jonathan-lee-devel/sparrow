6810ab6665ce76b5fd6bdbc752d33b42c7883cba
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-17 06:35:49 +0000

    Temporarily removed Dinero amounts for products

--- services/api/src/controllers/products/callbacks/create-product.ts
+++ services/api/src/controllers/products/callbacks/create-product.ts
@@ -7,8 +7,6 @@ import {Organization} from '../../../models/organizations/Organization';
 import {Product} from '../../../models/products/Product';
 import {HttpStatus} from '../../../lib/enums/HttpStatus';
 import {ModelTransformFunction} from '../../../lib/model-transform/default-model-transform';
-import Dinero, {Currency} from 'dinero.js';
-import {DEFAULT_PRECISION} from '../../../constants/products/dinero-constants';
 
 export const makeCreateProductCallback = (
     logger: winston.Logger,
@@ -18,16 +16,16 @@ export const makeCreateProductCallback = (
     transform: ModelTransformFunction,
 ): AuthenticatedEndpointCallback<CreateProductRequestBody, CreateProductRequestQuery> => async (req, res) => {
   const requestingUserEmail = req.user.email;
-  const {name, organizationId, priceAmount, priceCurrency} = req.body;
+  const {name, organizationId/* , priceAmount, priceCurrency*/} = req.body;
   logger.info(`Request from <${requestingUserEmail}> to create product with name: ${name} for organization with ID: ${organizationId}`);
   // @ts-ignore
-  const convertedCurrency: Currency = priceCurrency;
+  /* const convertedCurrency: Currency = priceCurrency;
   try {
     Dinero({amount: priceAmount, currency: convertedCurrency, precision: DEFAULT_PRECISION});
   } catch (err) {
     logger.error(err);
     return res.status(HttpStatus.BAD_REQUEST).json({error: `Invalid currency/amount provided for product price`});
-  }
+  }*/
 
   const organization = await Organization.findOne({id: organizationId}).exec();
   if (!organization) {
@@ -38,7 +36,7 @@ export const makeCreateProductCallback = (
     return res.status(HttpStatus.FORBIDDEN).send();
   }
 
-  const product = await Product.create({id: await generateId(), name, organizationId, priceAmount, priceCurrency});
+  const product = await Product.create({id: await generateId(), name, organizationId, priceAmount: 0.00, priceCurrency: 'EUR'});
 
   return res.status(HttpStatus.CREATED).json(product.toJSON({transform}));
 };
